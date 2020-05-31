60b5429265e6afc667dfffdd113fa1f4ca4ecef0
Ross Light <ross@zombiezen.com>
2020-05-31 14:43:13 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tailscale Chocolatey Package
+
+This is a [Chocolatey][] package of the [Tailscale][] client, the private
+networking solution.
+
+```
+choco install tailscale
+```
+
+[Chocolatey]: https://chocolatey.org/
+[Tailscale]: https://tailscale.com/
+
+## License
+
+The packaging scripts in this repository are released under the [Unlicense][].
+
+The Tailscale client is released under its [Terms of Service][].
+
+[Unlicense]: https://unlicense.org/
+[Terms of Service]: https://tailscale.com/terms
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,40 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-0.98-0.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'Tailscale IPN'
+
+  checksum      = '93960631223d41d96758000be7fdeeede72f88fc092686c6d33763f9fe54d406'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
