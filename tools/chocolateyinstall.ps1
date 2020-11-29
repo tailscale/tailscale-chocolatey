0a82f5fbcf8b85fd57edc07bb628ed70c6d87793
Duncan <legowerewolf@gmail.com>
2020-11-29 12:12:22 -0800

    Update install script to 1.2.9

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,24 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChildItem -Path "$toolsDir\tailscale.cer" | Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
 
-$url        = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.4.0.exe'
+$url = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.4.0.exe'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
 
-  softwareName  = 'Tailscale'
+  softwareName   = 'Tailscale'
 
-  checksum      = '90d66417e89ff16c0bdb76b991206467194e93daa732425160204db354d8a7ac'
-  checksumType  = 'sha256'
+  checksum       = '90d66417e89ff16c0bdb76b991206467194e93daa732425160204db354d8a7ac'
+  checksumType   = 'sha256'
 
-  silentArgs   = '/S'
-  validExitCodes= @(0)
+  silentArgs     = '/S'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
