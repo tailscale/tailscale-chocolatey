2a27e119d2428aa2937f34252d92d4203da48e93
Duncan Gibson <legowerewolf@gmail.com>
2021-05-19 16:13:04 -0700

    automatically get versions

--- updater.ps1
+++ updater.ps1
@@ -1,6 +1,10 @@
-$version = "1.8.3"
+$channel = "https://pkgs.tailscale.com/stable/"
 
-$url = "https://pkgs.tailscale.com/stable/tailscale-ipn-setup-$version.exe"
+$url = $channel + ((Invoke-WebRequest $channel).links | where-object { $_.href -like "*.exe" }).href
+
+$url -match "\d*\.\d*\.\d*"
+$version = $Matches[0]
+
 $hash = (Get-FileHash -Algorithm SHA256 -InputStream ([System.Net.WebClient]::new().OpenRead($url))).Hash.ToLower()
 
 # Update the nuspec
