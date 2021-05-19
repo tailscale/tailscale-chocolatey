0eb95f0a9600d02e91923b8463b5e4689d13989a
Duncan Gibson <legowerewolf@gmail.com>
2021-05-19 16:14:42 -0700

    ignore -match output

--- updater.ps1
+++ updater.ps1
@@ -2,7 +2,7 @@ $channel = "https://pkgs.tailscale.com/stable/"
 
 $url = $channel + ((Invoke-WebRequest $channel).links | where-object { $_.href -like "*.exe" }).href
 
-$url -match "\d*\.\d*\.\d*"
+$null = $url -match "\d*\.\d*\.\d*"
 $version = $Matches[0]
 
 $hash = (Get-FileHash -Algorithm SHA256 -InputStream ([System.Net.WebClient]::new().OpenRead($url))).Hash.ToLower()
