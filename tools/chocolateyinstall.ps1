5d3453fd088a365cf8828fc090cb5944d5578a25
Duncan <legowerewolf@gmail.com>
2021-05-24 07:37:34 -0700

    Update to v1.8.5 (#11)
    * Bump to v1.8.5
    
    * Use JSON for getting update info

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChildItem -Path "$toolsDir\tailscale.cer" | Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
 
-$url = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.8.3.exe'
+$url = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.8.5.exe'
 
 
 $packageArgs = @{
@@ -15,7 +15,7 @@ $packageArgs = @{
 
   softwareName   = 'Tailscale'
 
-  checksum       = 'd20a3e16ca885b2c1e4f65614ba5208d592ff09be9c9288e12be9affb87a3d01'
+  checksum       = 'eae1328cb6e3e19301a9e7d68830ac69f88b74456958e381dd5080b8c509953b'
   checksumType   = 'sha256'
 
   silentArgs     = '/S'
