b681e85cbfd0fa99b27133cba4681c30b7e86291
Duncan <legowerewolf@gmail.com>
2021-08-03 19:23:44 -0700

    Bump to v1.12.2 (#23)

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChildItem -Path "$toolsDir\tailscale.cer" | Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
 
-$url = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.12.1.exe'
+$url = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.12.2.exe'
 
 
 $packageArgs = @{
@@ -15,7 +15,7 @@ $packageArgs = @{
 
   softwareName   = 'Tailscale'
 
-  checksum       = '75c860053fa5eb28011cd29a0b227b4a1c46706e0c89c06ccb1f7a345fbb536b'
+  checksum       = 'e9ad6ded03d82c291553a052cf1c622667b19ab03207716fa799b43f40f872a9'
   checksumType   = 'sha256'
 
   silentArgs     = '/S'
