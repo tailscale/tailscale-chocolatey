22b0a8318ee1dedbb25253a07bede3e3bc1a992d
DOOREI <doorei@gmail.com>
2022-04-23 18:15:34 +0900

    Update to version 1.24.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChildItem -Path "$toolsDir\tailscale.cer" | Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
 
-$url = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.22.2.exe'
+$url = 'https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.24.0.exe'
 
 
 $packageArgs = @{
@@ -15,7 +15,7 @@ $packageArgs = @{
 
   softwareName   = 'Tailscale'
 
-  checksum       = '47d24098ff439649fc3c59cfce3cd3ee9460817dd106df4255f36d7c1d1d6a43'
+  checksum       = 'c805eb8934d7d7ad27e98fc14e82b45a02926e41a7e8b89426c5a6b2277acd78'
   checksumType   = 'sha256'
 
   silentArgs     = '/S'
