db07501b3f4df194488f2d54b0e936c4baadcd12
Duncan Gibson <legowerewolf@gmail.com>
2023-02-26 14:55:53 -0500

    Update to version 1.36.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -17,20 +17,20 @@ $packageArgs = @{
 if ($env:PROCESSOR_IDENTIFIER.StartsWith('ARMv')) {
   # Windows on ARM
   $packageArgs += @{
-    Url          = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.36.1-arm64.msi'
-    Checksum     = '39d35ded950284d87f5fd498b7f88494ae6139506abe62aae6f6469beaa9007f'
+    Url          = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.36.2-arm64.msi'
+    Checksum     = 'f1ad8bc33cb942c6a39a9e0771e256892b5f8511aaba934fe8d9b6429ce96ed3'
     ChecksumType = 'sha256'
   }
 }
 else {
   # Windows on x86 or x64
   $packageArgs += @{
-    Url            = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.36.1-x86.msi'
-    Checksum       = '6507866c6aa8a7c1c981267836884ae390b01fba5f36d2a70c6c709845b1b9e1'
+    Url            = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.36.2-x86.msi'
+    Checksum       = '31b6b57acc40de1c72d5cde32e93b30a9d99c6baeea16c16c71fd73db6ca9e1d'
     ChecksumType   = 'sha256'
 
-    Url64Bit       = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.36.1-amd64.msi'
-    Checksum64     = 'cbb6452ad455a5dd7ac679a8ce83609faea836313968db60b1719e042251ebbc'
+    Url64Bit       = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.36.2-amd64.msi'
+    Checksum64     = '2ee3f08cbaa108574d44f820484fb9d8fb21eeae80e0cef2e1fbbe4e55db92bd'
     ChecksumType64 = 'sha256'
   }
 }
