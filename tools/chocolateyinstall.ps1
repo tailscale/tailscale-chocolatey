210d286c937318e4ac5518524762b3cce7295d16
Denton Gentry <dgentry@tailscale.com>
2023-11-15 11:40:44 -0800

    Update to version 1.54.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -17,20 +17,20 @@ $packageArgs = @{
 if ($env:PROCESSOR_IDENTIFIER.StartsWith('ARMv')) {
   # Windows on ARM
   $packageArgs += @{
-    Url          = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.52.1-arm64.msi'
-    Checksum     = 'd6dac2486b2324441b79f7d3104d7bfe1cfb4178cc71442a04797462565ddfdc'
+    Url          = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.54.0-arm64.msi'
+    Checksum     = 'e4bc373591f08dda14c7d33719414a0585869ce636b6db5e7d9daae5b9c3e0ba'
     ChecksumType = 'sha256'
   }
 }
 else {
   # Windows on x86 or x64
   $packageArgs += @{
-    Url            = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.52.1-x86.msi'
-    Checksum       = '65c985d5da0aab8074b7ac632fb54b6ea420d678cc575c9971b86b2d5d00397e'
+    Url            = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.54.0-x86.msi'
+    Checksum       = '3b5b9b1bc831a02c26336cfe9286b4e7c4bc05fdb35426a6122760874c377e63'
     ChecksumType   = 'sha256'
 
-    Url64Bit       = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.52.1-amd64.msi'
-    Checksum64     = '78f3da14f77185608374b3b70ca3e43d39d4c58f073ba8e22e4347c2f6a746ae'
+    Url64Bit       = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.54.0-amd64.msi'
+    Checksum64     = 'd932e2db675d71cd483e5a29706af33ee93886b91092a612c7cec32a39e8bf16'
     ChecksumType64 = 'sha256'
   }
 }
