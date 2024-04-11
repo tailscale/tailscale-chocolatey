82e74e96d4ebf512c486065ad01174ecaf529b0e
Jenny Zhang <jz@tailscale.com>
2024-04-11 21:53:13 +0000

    Update to version 1.64.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -15,20 +15,20 @@ $packageArgs = @{
 if ($env:PROCESSOR_IDENTIFIER.StartsWith('ARMv')) {
   # Windows on ARM
   $packageArgs += @{
-    Url          = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.62.1-arm64.msi'
-    Checksum     = 'c07ffeed23a891f459cb3fa475e5106e06e5e3a4a8dc963f47f52c45e9b4ca5d'
+    Url          = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.64.0-arm64.msi'
+    Checksum     = '2113a42904168ea49d49a5fec34fb0e84d043e3e7ed227fc4a842f62874fd544'
     ChecksumType = 'sha256'
   }
 }
 else {
   # Windows on x86 or x64
   $packageArgs += @{
-    Url            = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.62.1-x86.msi'
-    Checksum       = '5cf9066b692f131927780ec16edbf0cfc4a02949782d349c3c1ee728c224dec7'
+    Url            = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.64.0-x86.msi'
+    Checksum       = 'f6bdac3aa1f65341f988a667ec3cb5122df4f190fd72582020109a073b7b8d62'
     ChecksumType   = 'sha256'
 
-    Url64Bit       = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.62.1-amd64.msi'
-    Checksum64     = 'f02c0c69c64c3df24ff59f7978523cc1b3c3c73d37019118a0aa197513e1c27e'
+    Url64Bit       = 'https://pkgs.tailscale.com/stable/tailscale-setup-1.64.0-amd64.msi'
+    Checksum64     = '46e61cd7037887f7049373545f07ba31dafaaf213299ae35fc8a5073332cfcc8'
     ChecksumType64 = 'sha256'
   }
 }
